3c71c67e3d86d83ed55456efc82e7156b13fd294
Danigs2301 <danielgs1219@gmail.com>
2026-05-20 18:53:14 -0500

    First push with basic structure and git ignore configuration

---
+++ Inventario/InventarioApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+var assembly = Assembly.GetExecutingAssembly();
+var version = assembly.GetName().Version;
+
+Console.WriteLine($"Platform: {Environment.OSVersion}");
+Console.WriteLine($".NET Version: {Environment.Version}");
+Console.WriteLine($"Application Version: {version}");
+Console.WriteLine($"Estructura del proyecto:");
+Console.WriteLine($"Configuracion .csproj:");
+Console.WriteLine($"Carpeta src/ creada:");
+Console.WriteLine($"Metadatos configurados:");
+Console.WriteLine($"Estructura del proyecto:");
+Console.WriteLine($"Estructura del proyecto:");
+Console.WriteLine($"Estructura del proyecto:");
